203334229629426e0a958f01631d541519f05021
Michael Cheng <michael@michaelcheng.us>
2015-06-03 21:43:04 +0800

    Basic features working

--- http.js
+++ http.js
@@ -13,24 +13,65 @@
 
 function $http(url) {
 	function ajax(method, url, args) {
-		console.log(method, url, args);
-		return callbacks;
+		var uri = (function encode(url, args) {
+			var out = url + "?";
+			var arg_cnt = 0;
+			for(var key in args) {
+				if(args.hasOwnProperty(key)) {
+					if(arg_cnt++) {
+						out += "&";
+					} 
+					out += encodeURIComponent(key) + "=" + encodeURIComponent(args[key]);
+				}
+			}
+			return out;
+		})(url, args);
+
+
+		$http.prototype.client.open(method, uri, true);
+		$http.prototype.client.send();
+		$http.prototype.client.onreadystatechange = function() {
+			callbacks.stateChanged($http.prototype.client.readyState)
+
+
+			if($http.prototype.client.readyState == 4) {
+				var response;
+				try {
+					response = JSON.parse($http.prototype.client.responseText);
+				} catch(e) {
+					response = $http.prototype.client.responseText;
+				}
+				callbacks.success(response);
+
+				// destroy everything
+				response = null;
+				url = null;
+				this.userCallbacks = null;
+				this.callbacks = null;
+			}
+		};
+
+		return userCallbacks;
 	};
 
-	var callbacks = {
+	
+
+	var userCallbacks = {
 		stateChanged: function(callback) {
-			callback(_state);
+			callbacks.stateChanged = callback;
 			return this;
 		},
 		success: function(callback) {
-			callback(_response);
+			callbacks.success = callback;
 			return this;
 		},
-		error: function() {
-			callback(_error);
+		error: function(callback) {
+			callbacks.error = callback;
 			return this;
 		}
 	};
+
+	var callbacks = {};
 
 	return {
 		'get': function(args) {
@@ -46,4 +87,6 @@ function $http(url) {
 			return ajax('delete', url, args);
 		}
 	};
-}
+};
+
+$http.prototype.client = window.ActiveXObject ? new ActiveXObject('Microsoft.XMLHTTP') : new XMLHttpRequest;
